ee18d479191e1cb65f268a1b91cd8851663f4dfb
yuvaharish <yuvaharish@gmail.com>
2023-08-01 01:31:47 +0530

    headless

--- src/test/java/com/steps/Hook.java
+++ src/test/java/com/steps/Hook.java
@@ -3,6 +3,7 @@ package com.steps;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import com.baseclass.BaseClass;
 
@@ -12,6 +13,8 @@ import io.cucumber.java.Before;
 public class Hook extends BaseClass{
 	@Before
 	public void loadDriver(){
+		ChromeOptions option=new ChromeOptions();
+		option.setHeadless(true);
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\jacki\\git\\FirstTry\\Driver\\chromedriver.exe");
 		driver= new ChromeDriver();
 		driver.manage().window().maximize();
